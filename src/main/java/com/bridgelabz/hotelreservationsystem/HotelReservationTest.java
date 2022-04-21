b542ae7d05d3fb8e0293a1b43b92c6f60acc29d5
AshokKoda <a.koda59@gmail.com>
2022-04-21 19:16:38 +0530

    Added:UC3 - Add weekdays and weekend rates for each hotel

--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
@@ -6,16 +6,25 @@ import org.junit.Test;
 public class HotelReservationTest {
 
 	@Test
-    public void givenHotelNamesWhenAddedShouldReturnSize() {
-        HotelReservation hotel = new HotelReservation();
-        hotel.addHotel();
-        Assert.assertEquals(3, hotel.hotelReservationList.size());
-    }
+	public void givenHotelNamesWhenAddedShouldReturnSize() {
+		HotelReservation hotel = new HotelReservation();
+		hotel.addHotel();
+		Assert.assertEquals(3, hotel.hotelReservationList.size());
+	}
 
-    @Test
-    public void givenDateRangeShouldReturnTheCheapestHotelRate() {
-        HotelReservation obj = new HotelReservation();
-        obj.addHotel();
-        Assert.assertEquals(220, obj.findCheapestHotel("2020-09-10", "2020-09-11"));
-    }
+	@Test
+	public void givenDateRangeShouldReturnTheCheapestHotelRate() {
+		HotelReservation obj = new HotelReservation();
+		obj.addHotel();
+		Assert.assertEquals(220, obj.findCheapestHotel("2020-09-10", "2020-09-11"));
+	}
+
+	@Test
+	public void givenWeekDayWeekEndRatesShouldReturnThoseRates() {
+		HotelReservation hotel = new HotelReservation();
+		hotel.addHotel();
+		Assert.assertEquals(90, hotel.hotelReservationList.get("Lakewood").getWeekendRate());
+		Assert.assertEquals(50, hotel.hotelReservationList.get("Bridgewood").getWeekendRate());
+		Assert.assertEquals(150, hotel.hotelReservationList.get("Ridgewood").getWeekendRate());
+	}
 }
